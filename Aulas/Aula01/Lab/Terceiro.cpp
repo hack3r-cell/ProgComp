2398b098a56c0154327fc45a7ef2b892a1e778bc
hack3r-cell <cellogine602@gmail.com>
2025-05-02 16:53:33 -0300

    Add files via upload

---
+++ Aulas/Aula01/Lab/Terceiro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int dias;     // declara uma variável inteira
+	cout << "Numero de Dias: ";
+	cin >> dias;  // lê um valor para a variável
+
+	int horas = dias * 60;
+	cout << dias << " dias tem " << horas << " horas." << endl;
+
+	return 0;
+}
