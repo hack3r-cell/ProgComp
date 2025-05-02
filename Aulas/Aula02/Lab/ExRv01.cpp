087f2e7f602db74c581159b6763eef5fd731e98a
hack3r-cell <cellogine602@gmail.com>
2025-05-02 17:10:33 -0300

    Add files via upload

---
+++ Aulas/Aula02/Lab/ExRv01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//cout << "Bem-vindo" << endl << "ao mundo" << endl << "do C++" << endl;
+	cout << "Bem-vindo\nao mundo\ndo C++\n";
+	return 0;
+}
