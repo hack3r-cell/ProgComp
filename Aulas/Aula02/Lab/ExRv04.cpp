fc02bedb0e9b03e3d22a1bcc3cbb35fb31d57522
hack3r-cell <cellogine602@gmail.com>
2025-05-02 17:23:33 -0300

    Add files via upload

---
+++ Aulas/Aula02/Lab/ExRv04.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	system("chcp 1252 > nul");
+
+	// exibindo uma frase que contém acentos
+	cout << "Acentuação é possível?" << endl;
+
+	// leia uma palavra que contém acentos
+	char palavra[40];
+	cin >> palavra;
+
+	// exibindo o que foi lido
+	cout << palavra << endl;
+	return 0;
+}
