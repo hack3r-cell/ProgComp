51c6ce89c1d2b5b96acd7e7c78bcbad73fdaab69
hack3r-cell <cellogine602@gmail.com>
2025-05-02 16:48:14 -0300

    Add files via upload

---
+++ Aulas/Aula01/Lab/Segundo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int cenouras;   // declara uma variável inteira
+	cenouras = 25;  // atribui um valor a uma variável
+
+	cout << "Eu tenho " << cenouras << " cenouras." << endl;
+	cenouras = cenouras - 1;
+	cout << "Agora eu tenho " << cenouras << " cenouras." << endl;
+	return 0;
+}
