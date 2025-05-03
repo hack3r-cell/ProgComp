6dd7255ee7435b0be9c12941ca156f86cf2b61a3
hack3r-cell <cellogine602@gmail.com>
2025-05-03 16:39:48 -0300

    Add files via upload

---
+++ Aulas/Aula02/Lab/ExFx01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	system("chcp 1252 > nul");
+	cout << "Roberto ";
+	cout << "Paiva";
+	cout << endl;
+	cout << "Rua Atenas, 50";
+	cout << endl;
+	cout << "Realengo, RJ";
+	cout << endl;
+	return 0;
+}
