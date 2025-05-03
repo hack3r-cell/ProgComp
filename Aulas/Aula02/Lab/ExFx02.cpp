373575a8ab65db10388ec3e2734ed88a83e141d3
hack3r-cell <cellogine602@gmail.com>
2025-05-03 16:46:30 -0300

    Add files via upload

---
+++ Aulas/Aula02/Lab/ExFx02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Roberto Paiva" << endl;
+	cout << "Roberto\nPaiva" << endl;
+	cout << "Roberto ";
+	cout << "Paiva";
+	cout << endl;
+	return 0;
+}
