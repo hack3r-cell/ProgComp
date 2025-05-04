24ba8c48d6978efa01df3ec0cc95d10768a096c3
hack3r-cell <cellogine602@gmail.com>
2025-05-04 15:24:47 -0300

    Add files via upload

---
+++ Aulas/Aula03/Lab/ExAp02.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	system("chcp 1252 > nul");
+	
+	cout << "Tabuada de n" << endl;
+	cout << "------------" << endl;
+	cout << "Entre com um número n (0 a 9): ";
+	int numero;
+
+	cin >> numero;
+
+	cout << numero << " x " << 0 << " = " << numero * 0 << endl;
+	cout << numero << " x " << 1 << " = " << numero * 1 << endl;
+	cout << numero << " x " << 2 << " = " << numero * 2 << endl;
+	cout << numero << " x " << 3 << " = " << numero * 3 << endl;
+	cout << numero << " x " << 4 << " = " << numero * 4 << endl;
+	cout << numero << " x " << 5 << " = " << numero * 5 << endl;
+	cout << numero << " x " << 6 << " = " << numero * 6 << endl;
+	cout << numero << " x " << 7 << " = " << numero * 7 << endl;
+	cout << numero << " x " << 8 << " = " << numero * 8 << endl;
+	cout << numero << " x " << 9 << " = " << numero * 9 << endl;
+
+	return 0;
+}
