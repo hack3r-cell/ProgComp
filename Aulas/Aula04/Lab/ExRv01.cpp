d91937a6fc4cc7796c5fcc9038cc4f428798de68
hack3r-cell <cellogine602@gmail.com>
2025-05-06 09:56:48 -0300

    Update ExRv01.cpp

--- Aulas/Aula04/Lab/ExRv01.cpp
+++ Aulas/Aula04/Lab/ExRv01.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 int main(void)
 {
+	system("chcp 1252 > nul");
+
+	cout << "Gerando números pseudoaleatórios: " << endl;
+	
 	srand(2);
-	cout << rand() << endl;
-	cout << rand() << endl;
-	cout << rand() << endl;
+	cout << rand() << " ";
+	cout << rand() << " ";
+	cout << rand() << " ";
 	cout << rand() << endl;
 
 	return 0;
