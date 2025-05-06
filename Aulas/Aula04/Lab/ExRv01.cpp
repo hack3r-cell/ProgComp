e9f3a87151c79fd2f7b15425be34b993097a0d21
hack3r-cell <cellogine602@gmail.com>
2025-05-06 09:55:08 -0300

    Add files via upload

---
+++ Aulas/Aula04/Lab/ExRv01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main(void)
+{
+	srand(2);
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << rand() << endl;
+	cout << rand() << endl;
+
+	return 0;
+}
