7591b9c060d09d52ad05e09d1fbe3a4ee109381e
hack3r-cell <cellogine602@gmail.com>
2025-05-09 09:49:54 -0300

    Add files via upload

---
+++ Aulas/Aula07/Lab/ExFx04.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+	system("chcp 1252 > nul");
+
+	std::cout << "Maça =\t\t20 Unidades" << std::endl;
+	std::cout << "Melancia =\t50 Unidades" << std::endl;
+	std::cout << "Ameixa =\t30 Unidades" << std::endl;
+
+	return 0;
+}
