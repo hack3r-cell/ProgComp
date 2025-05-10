1d871c5d25801fbaefcbc02e97ffe002b408680c
hack3r-cell <cellogine602@gmail.com>
2025-05-10 13:02:42 -0300

    Add files via upload

---
+++ Aulas/Aula08/Lab/ExFx05.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(void)
+{
+	// extrapola o número de dígitos significativos da mantissa
+	float f1 = 24980154.845f;
+
+	// extrapola o maior expoente possível
+	float f2 = 2E40f;
+
+	// extrapola o número de dígitos significativos da mantissa
+	double d1 = 293'849'384'958'473'847.394;
+
+	// extrapola o maior expoente possível
+	double d2 = 2E315;
+
+	return 0;
+}
