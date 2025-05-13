a45600c6d8d5268ada054807b20e88342a772edb
hack3r-cell <cellogine602@gmail.com>
2025-05-13 09:32:45 -0300

    Add files via upload

---
+++ Aulas/Aula09/Lab/ExFx02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void)
+{
+	system("chcp 1252 > nul");
+
+	std::cout << "Digite um ponto-flutuante: ";
+	float numero;
+	std::cin >> numero;
+
+	std::cout << "Notação padrão: " << numero << std::endl;
+	std::cout.setf(std::ios_base::scientific, std::ios_base::floatfield);
+	std::cout << "Notação científica: " << numero << std::endl;
+	std::cout.setf(std::ios_base::fixed, std::ios_base::floatfield);
+	std::cout.precision(2);
+	std::cout << "Notação decimal: " << numero << std::endl;
+
+	return 0;
+}
