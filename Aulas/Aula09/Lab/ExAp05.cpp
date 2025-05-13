e3c4c3e3c633d741eb44e9faf62754eae8bbcf7c
hack3r-cell <cellogine602@gmail.com>
2025-05-13 10:34:24 -0300

    Add files via upload

---
+++ Aulas/Aula09/Lab/ExAp05.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+	double numero = 245.795;
+	int numeroInt = int (numero);
+	
+	std::cout << numero * 100 << std::endl;
+	std::cout << numeroInt * 100 << std::endl;
+
+	return 0;
+}
