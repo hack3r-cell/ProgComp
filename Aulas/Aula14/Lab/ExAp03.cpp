1997c6ae618a721ff3cec3de694cf9eb7f61a4b2
hack3r-cell <cellogine602@gmail.com>
2025-05-20 08:09:31 -0300

    Add files via upload

---
+++ Aulas/Aula14/Lab/ExAp03.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+	int valor = 10, *temp, soma = 0;
+	temp = &valor;
+	*temp = 20;
+	temp = &soma;
+	*temp = valor;
+	std::cout << "valor: " << valor << "\nsoma: " << soma << std::endl;
+	return 0;
+}
