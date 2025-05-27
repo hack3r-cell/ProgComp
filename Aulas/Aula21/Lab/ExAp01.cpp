0c504e550ae7ed4e49c1bad092d9563fcd20707e
hack3r-cell <cellogine602@gmail.com>
2025-05-27 19:30:45 -0300

    Add files via upload

---
+++ Aulas/Aula21/Lab/ExAp01.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main(void)
+{
+	// a. ch não é um q nem um k
+	//char ch;
+	//if (ch != 'q' && ch != 'k')
+
+	// b. idade não está entre 15 e 16
+	//int idade = 20;
+	//if (!(idade >= 15 && idade <= 26))
+	//	std::cout << "Teste" << std::endl;
+
+	// c. x é impar e maior que 30
+	//int x = 36;
+	//if (x % 2 != 0 && x > 30)
+	//	std::cout << x << std::endl;
+
+	// d. num é multiplo de 5 ou de 8
+	//int num = 32;
+	//if (num % 5 == 0 || num % 8 == 0)
+	//	std::cout << num << std::endl;
+
+	// e. peso não é menor que 50 e altura é maior que 160
+	//int peso = 60;
+	//int altura = 170;
+	//if (!(peso < 50) && (altura > 160))
+	//	std::cout << "peso = " << peso << " altura = " << altura << std::endl;
+
+
+	return 0;
+}
